fc297379c37a4c361b8af65178d72302b56fcaee
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-24 14:26:36 +0000

    Added comments to break the problem down

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ from art import vs
 # print(vs)
 from game_data import data
 # print(data)
+
+#From dictionary named "data" import choice A and B
+choice_a = data(0)
+print(choice_a)
+#let the user make a guess
+
+#Compare the values 
+
+#If user is right make that choice choice A and add new choice B
+
+
+
