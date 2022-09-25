81040344749d4eefdd179bc187c1c255a71c1512
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-25 15:08:42 +0000

    def get_random_account()

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 from art import logo, vs
-print(logo)
 from replit import clear
 
 # print(vs)
@@ -15,10 +14,10 @@ from game_data import data
 #     },
 
 # Generate a random account from the game data.....1
-account_a = random.choice(data)
-account_b = random.choice(data)
-# print(account_a)
-# print(account_b)
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+    
 #Make sure the randomly chosen accounts are not the same.....2
 if account_a == account_b:
     account_b = random.choice(data)
@@ -47,22 +46,25 @@ follower_count_a = account_a["follower_count"]
 follower_count_b = account_b["follower_count"]
 
 #Set score.....7
+print(logo)
 score = 0
 #Set game status
 is_game_over = False
 
-
-# Check if user is correct.....6
-if guess == "a" and follower_count_a > follower_count_b:
-    score += 1
-    print(f"You win, your score is {score}")
-elif guess == "b" and follower_count_b > follower_count_a:
-    score += 1
-    print(f"You win, your score is {score}")
-elif guess == "a" and follower_count_a < follower_count_b:
-    print("You Lose")
-elif guess == "b" and follower_count_b < follower_count_a:
-    print("You Lose")
+while not is_game_over:
+    # Check if user is correct.....6
+    if guess == "a" and follower_count_a > follower_count_b:
+        score += 1
+        print(f"You win, your score is {score}")
+    elif guess == "b" and follower_count_b > follower_count_a:
+        score += 1
+        print(f"You win, your score is {score}")
+    elif guess == "a" and follower_count_a < follower_count_b:
+        is_game_over = True
+        print("You Lose")
+    elif guess == "b" and follower_count_b < follower_count_a:
+        is_game_over = True
+        print("You Lose")
     
 
 
